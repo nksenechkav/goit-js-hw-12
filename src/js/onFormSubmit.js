e0c43c7aca56f1870ba926238db6258784413104
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-02-15 17:01:02 +0100

    add Async

--- src/js/onFormSubmit.js
+++ src/js/onFormSubmit.js
@@ -5,16 +5,48 @@ import { fetchImg } from './fetchImage';
 import { loaderOn } from './loader';
 import { loaderOff } from './loader';
 
-export function onFormSubmit(event) {
-  event.preventDefault();
-  loaderOn();
-  refs.galleryList.innerHTML = '';
-  const userSearch = event.currentTarget.elements.input.value.trim();
+// let page = 1;
 
-  fetchImg(userSearch)
-      .then(makeGalleryItem)
-      .catch(onError)
-      .finally(loaderOff);
+// let limit = 15;
 
-  refs.form.reset();
-}
+// const totalPages = Math.ceil(100 / limit);
+
+
+// Btn.addEventListener("click", async () => {
+//     // Check the end of the collection to display an alert
+//     if (page > totalPages) {
+//       return iziToast.error({
+//         position: "topRight",
+//         message: "We're sorry, there are no more posts to load"
+//       });
+//     }
+  
+//     try {
+//       const posts = await fetchPosts();
+//       renderPosts(posts);
+//       // Increase the group number
+//       page += 1;
+  
+//       // Replace button text after first request
+//       if (page > 1) {
+//         Btn.textContent = "Fetch more posts";
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+
+
+  export function onFormSubmit(event) {
+    event.preventDefault();
+    loaderOn();
+    refs.galleryList.innerHTML = '';
+    const userSearch = event.currentTarget.elements.input.value.trim();
+  
+    fetchImg(userSearch)
+        .then(makeGalleryItem)
+        .catch(onError)
+        .finally(loaderOff);
+  
+    refs.form.reset();
+  }
